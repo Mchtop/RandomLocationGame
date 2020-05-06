4d456c78ec1e36e2625ddcaa6105eabbc3ecd893
Michael <michaeltoporkov99@gmail.com>
2020-05-06 22:15:48 +0200

    Some little changes kappa

--- Website/HTML/index.php
+++ Website/HTML/index.php
@@ -7,10 +7,14 @@
   <link rel="stylesheet" type="text/css" href="../CSS/test.css">
 </head>
 <body>
-  <div id="map"></div>
+  <div class="map-and-button">
+    <div id="map"></div>
+    <button id="guess-button">Guess</button>
+  </div>
   <div id="pano"></div>
+  <footer>
+    <div id="footer-bar"></div>
+  </footer>
   <script src="../JS/test.js"></script>
-  <script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaHZk4ZNggKV1Y8Dn80h3XkYbl6XAOmKw&callback=initialize">
-   </script>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaHZk4ZNggKV1Y8Dn80h3XkYbl6XAOmKw&callback=initialize"></script>
 </body>
