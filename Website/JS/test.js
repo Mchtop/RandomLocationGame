dc5861e2d08ca10127a9c48183d188c7e606c4fa
0yasum1 <36842990+0yasum1@users.noreply.github.com>
2020-05-06 20:05:23 +0200

    css changes

--- Website/JS/test.js
+++ Website/JS/test.js
@@ -2,7 +2,7 @@ function initialize() {
 var fenway = {lat: 42.345573, lng: -71.098326};
 var map = new google.maps.Map(document.getElementById('map'), {
     center: fenway,
-    zoom: 14
+    zoom: 1
 });
 var panorama = new google.maps.StreetViewPanorama(
     document.getElementById('pano'), {
