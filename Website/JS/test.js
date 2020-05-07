1a984764164197e8c54466373217f407902e4e2e
0yasum1 <36842990+0yasum1@users.noreply.github.com>
2020-05-07 09:42:17 +0200

    changes to js
    + finalized function to draw line between the markers
    + cleaned up the code

--- Website/JS/test.js
+++ Website/JS/test.js
@@ -1,99 +1,86 @@
 var map;
 var coordinates;
 var panorama;
-var marker;
-var marker2;
-var markerLocation;
+var markerActualLocation;
 var line;
 var markers = [];
-var latLng;
-var flightPlanCoordinates = [];
+var markerCoordinates = [];
 
 function initialize() {
 
   coordinates = {lat: 42.345573, lng: -71.098326};
 
+  //initializes simple map view
   map = new google.maps.Map(document.getElementById('map'), {
-      center: coordinates,
-      zoom: 1,
-      zoomControl: true,
-      mapTypeControl: false,
-      scaleControl: false,
-      streetViewControl: false,
-      rotateControl: false,
-      fullscreenControl: false
+    center: coordinates,
+    zoom: 1,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false,
+    draggableCursor:'crosshair'
   });
 
-  map.setOptions({draggableCursor:'crosshair'});
-
+  //initializes panorama view window
   panorama = new google.maps.StreetViewPanorama(
     document.getElementById('pano'), {
-        position: coordinates,
-        pov: {
+      position: coordinates,
+      pov: {
         heading: 34,
         pitch: 10
-        }
+      },
+      addressControl: false
     });
 
     google.maps.event.addListener(map, 'click', function(event) {
-       placeMarker(event.latLng);
+      placeMarker(event.latLng);
     });
 
+    //places a marker on click & moves the marker around
     function placeMarker(location) {
       if (!marker || !marker.setPosition) {
-        marker = new google.maps.Marker({
+        var marker = new google.maps.Marker({
           position: location,
           map: map,
         });
-        markerLocation = location;
+        markers.push(marker);
       } else {
-        markerLocation = location;
         marker.setPosition(location);
       }
     }
 
     google.maps.event.addDomListener(window, 'load', initialize);
-}
-
-// connect marker with actual location
-function guessFunction(){
-
-  marker2 = new google.maps.Marker({
-    position: coordinates,
-    map: map,
-  });
-
-  alert(marker + ", " + marker2);
-  markers.push(marker);
-  markers.push(marker2);
-
-  // coords.push(coordinates);
-  // coords.push(markerLocation);
-  //
-  // line = new google.maps.Polyline({
-  //   path: coords,
-  //   geodesic: true,
-  //   strokeColor: '#FF0000',
-  //   strokeOpacity: 1.0,
-  //   strokeWeight: 2
-  //   });
-  // line.setMap(map);
-
-  for(var marker in markers){
-    latLng = {
-      'lat':markers[marker].position.lat(),
-      'lng':markers[marker].position.lng()
-    };
-    flightPlanCoordinates.push(latLng);
   }
 
-  var flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2
-  });
+  /* function to place marker with coordinates of the location &
+   connect it to the marker placed by the user */
+  function guessFunction(){
+    markerActualLocation = new google.maps.Marker({
+      position: coordinates,
+      map: map,
+    });
 
-  flightPath.setMap(map);
-}
+    markers.push(markerActualLocation);
+
+    for(var marker in markers){
+      latLng = {
+        'lat':markers[marker].position.lat(),
+        'lng':markers[marker].position.lng()
+      };
+      markerCoordinates.push(latLng);
+    }
+
+    var line = new google.maps.Polyline({
+      path: markerCoordinates,
+
+      strokeColor: '#000000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+
+    line.setMap(map);
+
+    // disables moving the marker after clicking "guess"
+    google.maps.event.clearListeners(map, 'click');
+  }
