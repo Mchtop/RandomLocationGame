a4c0eb2e37af887c466bc04b8710fc0b7077c4c9
Michael <michaeltoporkov99@gmail.com>
2020-05-14 11:09:42 +0200

    changes

--- Website/HTML/index.php
+++ Website/HTML/index.php
@@ -7,11 +7,25 @@
   <link rel="stylesheet" type="text/css" href="../CSS/test.css">
 </head>
 <body>
-  <header>
-    <div id="header-bar">
-      <p id="result-text"></p>
-    </div>
+  <header id="header-bar">
+      <div id="header-next-game">
+        <input type="button" id="next-game-button" onClick="" value="Next Game"/>
+      </div>
+      <div id="header-result">
+        
+      </div>
+      <div id="header-score">
+        <p>X Points</p>
+      </div>
   </header>
+  <div class="result-popup-window">
+    <div id="result-popup">
+      <span id="result-text"></span>
+      <span id="X Points"></span>
+      <span>test</span>
+    </div>
+  </div>
+
   <script src="../JS/test.js"></script>
   <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaHZk4ZNggKV1Y8Dn80h3XkYbl6XAOmKw&callback=initialize"></script>
   <div class="map-and-button">
